a24b445354c92793886bfac0087dec05272b7a85
Mochammad Gamal <mogams1811@gmail.com>
2024-02-01 10:33:59 +0700

    chore: :hammer: intro what is json data

---
+++ app.js
@@ -0,0 +1,23 @@
+const data = `{
+    "success": true,
+    "terms": "https://coinlayer.com/terms",
+    "privacy": "https://coinlayer.com/privacy",
+    "timestamp": 1529571067,
+    "target": "USD",
+    "rates": {
+        "611": 0.389165,
+        "ABC": 59.99,
+        "ACP": 0.014931,
+        "ACT": 0.15927,
+        "ACT*": 0.14371,
+        "ADA": 0.160502,
+        "ADCN": 0.001406,
+        "ADL": 121.5,
+        "ADX": 0.427854,
+        "ADZ": 0.02908,
+        "AE": 2.551479,
+        "AGI": 0.12555,
+        "AIB": 0.005626,
+        "AIDOC": 0.02605
+    }
+}`;
