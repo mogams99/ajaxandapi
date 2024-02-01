ae4a968f2746baa71e066e2f09a10f288f534634
Mochammad Gamal <mogams1811@gmail.com>
2024-02-01 14:35:22 +0700

    chore: :hammer: simple exampe of xml http request

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
-const data = `{
-    "success": true,
-    "terms": "https://coinlayer.com/terms",
-    "privacy": "https://coinlayer.com/privacy",
-    "timestamp": 1529571067,
-    "target": "USD",
-    "rates": {
-        "611": 0.389165,
-        "ABC": 59.99,
-        "ACP": 0.014931,
-        "ACT": 0.15927,
-        "ACT*": 0.14371,
-        "ADA": 0.160502,
-        "ADCN": 0.001406,
-        "ADL": 121.5,
-        "ADX": 0.427854,
-        "ADZ": 0.02908,
-        "AE": 2.551479,
-        "AGI": 0.12555,
-        "AIB": 0.005626,
-        "AIDOC": 0.02605
-    }
-}`;
+// const data = `{
+//     "success": true,
+//     "terms": "https://coinlayer.com/terms",
+//     "privacy": "https://coinlayer.com/privacy",
+//     "timestamp": 1529571067,
+//     "target": "USD",
+//     "rates": {
+//         "611": 0.389165,
+//         "ABC": 59.99,
+//         "ACP": 0.014931,
+//         "ACT": 0.15927,
+//         "ACT*": 0.14371,
+//         "ADA": 0.160502,
+//         "ADCN": 0.001406,
+//         "ADL": 121.5,
+//         "ADX": 0.427854,
+//         "ADZ": 0.02908,
+//         "AE": 2.551479,
+//         "AGI": 0.12555,
+//         "AIB": 0.005626,
+//         "AIDOC": 0.02605
+//     }
+// }`;
+
+const req = new XMLHttpRequest();
+let data;
+req.onload = function () {
+    data = JSON.parse(this.response)
+    console.log(data);
+};
+req.onerror = function () {
+    console.error('message: ', this);
+};
+req.open('GET', 'https://swapi.dev/api/people/18');
+req.send();
