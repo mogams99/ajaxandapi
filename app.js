6167de785c4142281626bb36338528727b9b32a9
Mochammad Gamal <mogams1811@gmail.com>
2024-02-01 16:11:51 +0700

    chore: :hammer: implementasi get source data jokes bapack bapack

--- app.js
+++ app.js
@@ -79,3 +79,27 @@
 //         console.error(error.response.data);
 //     }
 // }
+
+// ! praktik get api bapack bapack2
+const jokes = document.querySelector('#jokes');
+const button = document.querySelector('button');
+const addJoke = async () => {
+    const jokeText = await getJokes();
+    const newLI = document.createElement('LI');
+    newLI.append(jokeText);
+    jokes.append(newLI);
+}
+const getJokes = async () => {
+    try {
+        const config = {
+            headers: {
+                Accept: 'application/json',
+            }
+        };
+        const res = await axios.get('https://icanhazdadjoke.com/', config);
+        return res.data.joke;
+    } catch (error) {
+        return "No Jokes Available!";
+    }
+}
+button.addEventListener('click', addJoke);
